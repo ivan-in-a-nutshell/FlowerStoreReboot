fe27cb6e782f1f0c562c2a59cca6aee45811cdf2
Ivan Zarytskyi <ivan.zarytskyi1991@gmail.com>
2025-11-13 20:12:16 +0200

    Fix

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -22,29 +22,46 @@ public class AppUser {
             strategy = GenerationType.SEQUENCE,
             generator = "user_sequence"
     )
-    @Getter
-    @Setter
     private Long id;
 
     @Column(
             nullable = false,
             unique = true
     )
-    @Getter
-    @Setter
     private String email;
 
     @Column(nullable = false)
-    @Getter
-    @Setter
     private LocalDate dob;
 
     @Transient
-    @Setter
     private Integer age;
 
     public AppUser(String email, LocalDate dob) {
         this.email = email;
+        this.dob = dob;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
         this.dob = dob;
     }
 
@@ -53,5 +70,9 @@ public class AppUser {
             return null;
         }
         return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
